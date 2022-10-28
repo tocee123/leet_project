0c7f9d045c759a48c4c4cf0a47b31b4b7d5501e5
Tamas Torok <tamas.torok@newday.co.uk>
2022-10-28 15:07:40 +0100

    New fikle

---
+++ LeetProject/Solution.ValidParentheses.cs
@@ -0,0 +1,46 @@
+﻿namespace LeetProject
+{
+    public partial class Solution
+    {
+        [Theory]
+        [InlineData(new[] { "flower", "flow", "flight" }, "fl")]
+        [InlineData(new[] { "dog", "racecar", "car" }, "")]
+        [InlineData(new[] { "dog", "dog", "dog" }, "dog")]
+        public void LongestCommonPrefix_Tests(string[] input, string expected)
+        {
+            LongestCommonPrefix1(input).Should().Be(expected);
+        }
+
+        public string LongestCommonPrefix(string[] strs)
+        {
+            string longestCommonPrefix = "";
+            var i = 1;
+            while (longestCommonPrefix != strs.First())
+            {
+                var commonPrefix = strs.First()[..i];
+                if (!strs.All(x => x.StartsWith(commonPrefix)))
+                {
+                    break;
+                }
+                i++;
+                longestCommonPrefix = commonPrefix;
+            }
+            return longestCommonPrefix;
+        }
+
+        public string LongestCommonPrefix1(string[] strs)
+        {
+            var minimum = strs.First().Length;
+
+            for (int i = 1; i < strs.Length; i++)
+            {
+                int j = 0;
+                while (j < strs[i].Length && strs[i][j] == strs[0][j])
+                    j++;
+                minimum = Math.Min(minimum, j);
+            }
+
+            return strs.First()[..minimum];
+        }
+    }
+}
